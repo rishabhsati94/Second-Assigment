311fe5d6674c6d48ba845cc5c7a35bb92dfd86f9
Rishabh Kumar Sati <rishabhsati94@gmail.com>
2021-05-30 14:04:23 +0530

    Sorting a Array

--- Second_Assigment/src/second/Second_Assigment.java
+++ Second_Assigment/src/second/Second_Assigment.java
@@ -1,5 +1,6 @@
 package second;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Second_Assigment {
@@ -7,18 +8,30 @@ public class Second_Assigment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Enter the Size of Array");
+		System.out.print("Enter the Size of Array:");
 		int size = s.nextInt();
+		
+		if(size == 0) {
+			System.out.println("Not Found");
+			return;
+		}
+		
 		int[] arr = new int[size+1];
 		for (int i =1; i < size+1; i++) {
 			System.out.print("Enter "+i+" Element: ");
 			arr[i] = s.nextInt();
-			System.out.println();
 		}
 		
-		System.out.println("Array is:");
+		System.out.println("Array is Before Sorting:");
 		for(int i = 1; i < size+1; i++) {
-			System.out.print(i+"\t");
+			System.out.print(arr[i]+"\t");
+		}
+		System.out.println();
+		Arrays.sort(arr);
+		
+		System.out.println("Array is After Sorting:");
+		for(int i = 1; i < size+1; i++) {
+			System.out.print(arr[i]+"\t");
 		}
 
 	}

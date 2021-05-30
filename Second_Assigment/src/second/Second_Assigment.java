09aee4e50fab691c522753c487654671127cf417
Rishabh Kumar Sati <rishabhsati94@gmail.com>
2021-05-30 13:08:22 +0530

    Declaration of Array

---
+++ Second_Assigment/src/second/Second_Assigment.java
@@ -0,0 +1,26 @@
+package second;
+
+import java.util.Scanner;
+
+public class Second_Assigment {
+	static Scanner s = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the Size of Array");
+		int size = s.nextInt();
+		int[] arr = new int[size+1];
+		for (int i =1; i < size+1; i++) {
+			System.out.print("Enter "+i+" Element: ");
+			arr[i] = s.nextInt();
+			System.out.println();
+		}
+		
+		System.out.println("Array is:");
+		for(int i = 1; i < size+1; i++) {
+			System.out.print(i+"\t");
+		}
+
+	}
+
+}

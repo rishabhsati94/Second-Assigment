9774101620090e19ed3703a7e7d4de92910ec954
Rishabh Kumar Sati <rishabhsati94@gmail.com>
2021-05-30 14:22:00 +0530

    RemoveDuplicate Elements from Array

--- Second_Assigment/src/second/Second_Assigment.java
+++ Second_Assigment/src/second/Second_Assigment.java
@@ -5,6 +5,36 @@ import java.util.Scanner;
 
 public class Second_Assigment {
 	static Scanner s = new Scanner(System.in);
+	
+	static int removeDuplicates(int arr[], int n)
+    {
+        // Return, if array is empty
+        // or contains a single element
+        if (n==0 || n==1)
+            return n;
+      
+        int[] temp = new int[n];
+         
+        // Start traversing elements
+        int j = 0;
+        for (int i=0; i<n-1; i++)
+            // If current element is not equal
+            // to next element then store that
+            // current element
+            if (arr[i] != arr[i+1])
+                temp[j++] = arr[i];
+         
+        // Store the last element as whether
+        // it is unique or repeated, it hasn't
+        // stored previously
+        temp[j++] = arr[n-1];  
+         
+        // Modify original array
+        for (int i=0; i<j; i++)
+            arr[i] = temp[i];
+      
+        return j;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -17,20 +47,22 @@ public class Second_Assigment {
 		}
 		
 		int[] arr = new int[size+1];
-		for (int i =1; i < size+1; i++) {
+		for (int i =1; i < arr.length; i++) {
 			System.out.print("Enter "+i+" Element: ");
 			arr[i] = s.nextInt();
 		}
 		
-		System.out.println("Array is Before Sorting:");
-		for(int i = 1; i < size+1; i++) {
-			System.out.print(arr[i]+"\t");
-		}
-		System.out.println();
+//		System.out.println("Array is Before Sorting:");
+//		for(int i = 1; i < arr.length; i++) {
+//			System.out.print(arr[i]+"\t");
+//		}
+//		System.out.println();
 		Arrays.sort(arr);
 		
+		int size2 = removeDuplicates(arr, (arr.length));
+		
 		System.out.println("Array is After Sorting:");
-		for(int i = 1; i < size+1; i++) {
+		for(int i = 1; i < size2; i++) {
 			System.out.print(arr[i]+"\t");
 		}
 
